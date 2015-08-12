39b3728eadc589d5df565aa554633f026095bbc3
Angel-Lara <espectro974@gmail.com>
2015-08-12 00:38:27 -0500

    Preguntas

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//importar paquetes con los middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var partials = require('express-partials');
+var partials = require('express-partials'); // Añadimos un nuevo módulo
+var methodOverride = require('method-override');
+var session = require('express-session');
 
-//importar routes
 var routes = require('./routes/index');
+//var users = require('./routes/users');   ** no lo vamos a usar **
 
-//generar aplicacion
 var app = express();
 
 // view engine setup
@@ -18,22 +18,55 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));  // Descomentamos esta línea cuando añadimos el favicon
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(partials());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-//asociar rutas a los gestores
+app.use(partials());  // Se invoca con () para generar el MW a instalar
+
+// Helpers dinamicos:
+app.use( function(req, res, next) {
+
+    // guardar path en session.redir para despues de login
+    if (!req.path.match(/\/login|\/logout/)) {
+        req.session.redir = req.path;
+    }
+
+    // Hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+});
+
+// Auto-logout
+app.use( function(req, res, next) {
+  if ( req.session.user ) {
+    var ahora = new Date();                           // ahora el tiempo es mayor
+    var activo = new Date( req.session.user.activo ); // momento cuando se hizo algo
+    if ( ( ahora-activo ) > 120000 ) {                // 120000ms = 2 segundos
+        delete req.session.user;
+        req.session.errors = [ { "message": 'Superado el tiempo de inactividad' } ];
+        res.redirect("/login");
+        return;
+    } else {
+        req.session.user.activo = new Date();
+    }
+  }
+  next();
+});
+
 app.use('/', routes);
+//app.use('/users', users);   ** no lo vamos a usar **
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -41,23 +74,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            errors: []
+        });
     });
-  });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        errors: []
+    });
 });
+
 
 module.exports = app;
