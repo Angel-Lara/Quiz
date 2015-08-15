a8804943c2f3d498d8981707d71f19215c4513ea
Angel-Lara <espectro974@gmail.com>
2015-08-15 16:59:46 -0500

    Final

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,118 @@
-exports.index = function(request, response){
-	 response.render('', {title: 'Bienvenid@ a Quiz', file: 'index'});
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req,res,next,quizId){
+    models.Quiz.find({
+        where: {id: Number(quizId)},
+        include: [{model: models.Comment}]
+    }).then(function(quiz){
+            if (quiz) {
+                req.quiz = quiz;
+                next();
+            }else{ next(new Error('No existe quizId=' + quizId));}
+        }
+        ).catch(function(error){next(error);});
+};
+
+// GET /quizes
+exports.index = function(req,res){
+    if (req.query.search){
+         var busqueda = req.query.search.replace(/\s+/g, "%");
+        
+
+         models.Quiz.findAll({where: ["upper(pregunta) like ?", "%" + busqueda.toUpperCase() + "%"],order: 'pregunta ASC'}).then(
+
+        function(quizes){
+            res.render('quizes/index',{quizes:quizes, errors: []});
+        }
+        ).catch(function(error){next(error);})
+        
+    }else{
+        models.Quiz.findAll().then(
+            function(quizes){
+            res.render('quizes/index.ejs',{quizes:quizes,errors: []});
+        }
+        ).catch(function(error) {next(error);});
+       
+    }
+};
+
+// GET /quizes/:id
+exports.show = function(req,res){
+        res.render('quizes/show',{quiz:req.quiz,errors:[]});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req,res){
+   var resultado = 'Incorrecto'; 
+        if (req.query.respuesta === req.quiz.respuesta){
+            resultado='Correcto';
+        }
+    res.render('quizes/answer',{quiz:req.quiz,respuesta: resultado, errors: []});
+};
+      
+exports.author = function(req,res){
+    res.render('autor',{autor:'Angel Lara',errors: []});
+};
+
+// GET /quizes/new
+exports.new = function(req,res){
+    var quiz = models.Quiz.build( // crea objeto quiz
+        {pregunta:"Pregunta",respuesta:"Respuesta",tema:"Tema"}
+        );
+    res.render('quizes/new',{quiz:quiz,errors:[]});
+};
+
+// POST /quizes/create
+exports.create = function(req,res){
+    var quiz = models.Quiz.build(req.body.quiz);
+    
+    quiz
+    .validate()
+    .then(
+        function(err){
+            if(err){
+                res.render('quizes/new',{quiz:quiz,errors:err.errors});
+            } else {
+    // guardar en DB los campos pregunta y respuesta de quiz
+    quiz
+    .save({fields:["pregunta","respuesta","tema"]})
+    .then(function(){res.redirect('/quizes')}) 
+    // Redireccion HTTP (URL relativo) lista de preguntas
 }
+        }
+        );
+};
 
+// GET /quizes/:id/edit
+exports.edit = function(req,res){
+    var quiz = req.quiz; // autoload de instancia quiz
+    res.render('quizes/edit',{quiz:quiz,errors:[]});
+};
 
-exports.question = function(request, response){
-	 response.render('quizes/question', {title: 'Bienvenid@ a Quiz', question: 'Capital de Francia', file: 'quizes/question'});
-}
-
-exports.answer = function(request, response){
-
-	var answer = request.query.answer;
-	var feedback = answer && answer.toLowerCase() === "paris" ? "Correcto" : "Incorrecto";	
-
-	response.render('quizes/answer', { title: 'Bienvenid@ a Quiz', feedback: feedback, file : 'quizes/answer' });
-}
-
-exports.author = function(request, response){
-	response.render('author', { title: 'Bienvenid@ a Quiz', file: 'author' });
-}
-
-exports.error = function(request, response) {
-	response.render('error', { title: 'ERROR 404', file : 'error'});
-}
+// PUT /quizes/:id
+exports.update = function(req,res){
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
+    
+    req.quiz
+    .validate()
+    .then(
+        function(err){
+            if (err){
+                res.render('quizes/edit',{quiz:req.quiz,errors:err.errors});
+            }else{
+                req.quiz // save : guardar campos preunta y respuesta en BD
+                .save({fields:["pregunta","respuesta","tema"]})
+                .then( function(){res.redirect('/quizes');});
+            } // redireccion a HTTP a lista de preguntas
+        }
+        );
+};
+// DELETE /quizes/:id
+exports.destroy = function(req,res){
+    req.quiz.destroy().then( function(){
+        res.redirect('/quizes');
+    }).catch(function(error){next(error)});
+};
